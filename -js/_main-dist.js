d16b270edb56113a216989a92e2ebea7e9e6074b
JonyDroid <ijony.droid@gmail.com>
2017-08-10 01:27:51 -0500

    Estructura HTML

---
+++ -js/_main-dist.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$("input.search").focus(function(){$(".result-search").css("display","block").show(2e3)}),$("input.search").blur(function(){$(".result-search").css("display","none").fadeOut(2e3)})});
+//# sourceMappingURL=_main-dist.js.map
