b7560535ee4b55808e2f27db8dcb41bea1198c3b
jrestrepo <jrestrepo@chefcompany.co>
2018-08-22 20:02:49 -0500

    change framework --> Bootstrap

--- -js/_main-dist.js
+++
@@ -1,2 +0,0 @@
-$(document).ready(function(){$("input.search").focus(function(){$(".result-search").css("display","block").show(2e3)}),$("input.search").blur(function(){$(".result-search").css("display","none").fadeOut(2e3)})});
-//# sourceMappingURL=_main-dist.js.map
--- -js/heder.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("input.search").focus(function() {
-    $(".result-search").css("display", "block").show(2000);
-  });
-  $("input.search").blur(function() {
-    $(".result-search").css("display", "none").fadeOut(2000);
-  });
-});
